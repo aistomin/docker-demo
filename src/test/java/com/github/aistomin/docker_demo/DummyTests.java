78562d16013dff6a80ef24467e8499fcc65662c6
aistomin <andrej.istomin.ikeen@gmail.com>
2024-11-07 09:04:04 +0100

    Issue #1. Application skeleton

---
+++ src/test/java/com/github/aistomin/docker_demo/DummyTests.java
@@ -0,0 +1,12 @@
+package com.github.aistomin.docker_demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTests {
+
+    @Test
+    void testDummy() {
+        Assertions.assertEquals(4, 2 + 2);
+    }
+}
